057f1733bdb1eabeaa2f1c85cdaa2d12a99efdc5
Anton Maisak <anton@maisak.ru>
2020-08-16 12:46:09 +0300

    Added width and height support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+import { NgxBootstrapIconsModule } from '../../projects/ngx-bootstrap-icons-lib/src/lib/ngx-bootstrap-icons.module';
+import { allIcons} from '../../projects/ngx-bootstrap-icons-lib/src/lib/icons/all';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
