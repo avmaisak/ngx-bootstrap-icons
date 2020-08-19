8ee8c5cfdcd8031e4df13ef6d7e31d4b89dd5181
Anton Maisak <anton@maisak.ru>
2020-08-19 10:54:01 +0300

    updated to angular 10.0.10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import ru from '@angular/common/locales/ru';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { allIcons, NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
-import { allIcons} from 'ngx-bootstrap-icons';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+registerLocaleData(ru);
 
 @NgModule({
   declarations: [
