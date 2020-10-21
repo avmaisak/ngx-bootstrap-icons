918ba68d36be734cd2f2c57247299a07ff5af96f
Anton Maisak <anton@maisak.ru>
2020-10-21 23:38:46 +0300

    Added version to Demo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { version } from '../../../package.json';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  public get version(): string {
+    return version;
+  }
 }
