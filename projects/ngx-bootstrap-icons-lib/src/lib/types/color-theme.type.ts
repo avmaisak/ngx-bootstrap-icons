948bfe9e1626ab7b43bab50e42f1367773fac80d
tayamba mwanza <tayamba@ipventures.com.au>
2021-07-11 23:19:50 +0200

    feat: theme type

--- projects/ngx-bootstrap-icons-lib/src/lib/types/color-theme.type.ts
+++ projects/ngx-bootstrap-icons-lib/src/lib/types/color-theme.type.ts
@@ -3,15 +3,15 @@
  *
  * {@link https://getbootstrap.com/docs/5.0/utilities/colors/#color}
  */
-export type ColorTheme =
-'text-primary' |
-'text-secondary' |
-'text-success' |
-'text-danger' |
-'text-warning' |
-'text-info' |
-'text-dark' |
-'text-body' |
-'text-muted' |
-'text-black-50' |
-'text-white-50';
+export type ColorThemeType =
+  | "text-primary"
+  | "text-secondary"
+  | "text-success"
+  | "text-danger"
+  | "text-warning"
+  | "text-info"
+  | "text-dark"
+  | "text-body"
+  | "text-muted"
+  | "text-black-50"
+  | "text-white-50";
