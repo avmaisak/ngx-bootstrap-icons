e46df5658f44946311e00dbabac278ac7ea2380e
tayamba mwanza <tayamba@ipventures.com.au>
2021-07-11 13:07:32 +0200

    feat: generate icons

---
+++ projects/ngx-bootstrap-icons-lib/src/lib/types/color-theme.type.ts
@@ -0,0 +1,17 @@
+/**
+  Color Themes.
+ *
+ * {@link https://getbootstrap.com/docs/5.0/utilities/colors/#color}
+ */
+export type ColorTheme =
+'text-primary' |
+'text-secondary' |
+'text-success' |
+'text-danger' |
+'text-warning' |
+'text-info' |
+'text-dark' |
+'text-body' |
+'text-muted' |
+'text-black-50' |
+'text-white-50';
--- tools/generate-icons.js
+++ tools/generate-icons.js
@@ -10,7 +10,7 @@ const allIconsVariable = 'allIcons';
 const libFolder = 'projects/ngx-bootstrap-icons-lib/src/lib';
 
 // source original bootstrap icons
-const iconsSrcFolder = "../icons/icons";
+const iconsSrcFolder = "../icons";
 // enum output folder
 const enumDestFolder = `${libFolder}/enums`;
 // type output folder
@@ -35,49 +35,38 @@ let exportTypeString = `/** Type for icon names. */`;
 exportEnumString += `\nexport enum IconNamesEnum {\n`;
 exportTypeString += `\nexport type IconName =\n`;
 
-return Promise.resolve()
-  .then(() => fs.emptyDirSync(iconsDestFolder))
-  .then(() => {
-    fs.readdirSync(iconsSrcFolder).forEach((filename) => {
-
-      const iconName = filename.replace(".svg", "").trim();
-      const fileContent = fs.readFileSync(
-        `${iconsSrcFolder}/${filename}`,
-        "utf-8"
-      );
-      const exportName = camelcase(iconName);
-
-      exportEnumString += `  /** ${urlBase}${iconName} */\n`;
-      exportEnumString += `  ${uppercamelcase(iconName)} = '${iconName}',\n`;
-
-      exportTypeString += `  /** https://icons.getbootstrap.com/icons/${iconName} */\n`;
-      exportTypeString += `'${iconName}' |\n`;
-
-      let output = componentTemplate
-        .replace(/__ICON_NAME__/g, iconName)
-        .replace(/__EXPORT_NAME__/g, exportName)
-        .replace(/__PAYLOAD__/, fileContent);
-
-      fs.writeFileSync(`${iconsDestFolder}/${iconName}.ts`, output, "utf-8");
-      fs.appendFileSync(
-        indexFile,
-        `export { ${exportName} } from './${iconName}';\n`
-      );
-      fs.appendFileSync(
-        allFile,
-        `import { ${exportName} } from './${iconName}';\n`
-      );
-      exportAllString += `  ${exportName},\n`;
-
-      console.log(`icon ${clc.green(exportName)} generated.`);
-    });
-
-    exportAllString += `};\n`;
-    exportEnumString += `}\n`;
-    exportTypeString += `}\n`;
-
-    fs.appendFileSync(allFile, exportAllString);
-    fs.appendFileSync(indexFile, `\nexport { ${allIconsVariable} } from './all';\n`);
-    fs.writeFileSync(enumFile, exportEnumString);
-    fs.writeFileSync(typeFile, exportTypeString);
-  });
+fs.emptyDirSync(iconsDestFolder);
+fs.readdirSync(iconsSrcFolder).forEach((filename) => {
+  const iconName = filename.replace(".svg", "").trim();
+  const fileContent = fs.readFileSync(
+    `${iconsSrcFolder}/${filename}`,
+    "utf-8"
+  );
+  const exportName = camelcase(iconName);
+  exportEnumString += `  /** ${urlBase}${iconName} */\n`;
+  exportEnumString += `  ${uppercamelcase(iconName)} = '${iconName}',\n`;
+  exportTypeString += `  /** https://icons.getbootstrap.com/icons/${iconName} */\n`;
+  exportTypeString += `'${iconName}' |\n`;
+  let output = componentTemplate
+    .replace(/__ICON_NAME__/g, iconName)
+    .replace(/__EXPORT_NAME__/g, exportName)
+    .replace(/__PAYLOAD__/, fileContent);
+  fs.writeFileSync(`${iconsDestFolder}/${iconName}.ts`, output, "utf-8");
+  fs.appendFileSync(
+    indexFile,
+    `export { ${exportName} } from './${iconName}';\n`
+  );
+  fs.appendFileSync(
+    allFile,
+    `import { ${exportName} } from './${iconName}';\n`
+  );
+  exportAllString += `  ${exportName},\n`;
+  console.log(`icon ${clc.green(exportName)} generated.`);
+});
+exportAllString += `};\n`;
+exportEnumString += `};\n`;
+exportTypeString += `;\n`;
+fs.appendFileSync(allFile, exportAllString);
+fs.appendFileSync(indexFile, `\nexport { ${allIconsVariable} } from './all';\n`);
+fs.writeFileSync(enumFile, exportEnumString);
+fs.writeFileSync(typeFile, exportTypeString);
