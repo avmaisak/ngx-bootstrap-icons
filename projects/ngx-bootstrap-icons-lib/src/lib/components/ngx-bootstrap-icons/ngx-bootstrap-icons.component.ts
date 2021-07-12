582d8aea9ffcd193beed97871e8117405be8a1a3
tayamba mwanza <tayamba@ipventures.com.au>
2021-07-12 09:27:52 +0200

    refactor: fix formatting issues

--- projects/ngx-bootstrap-icons-lib/src/lib/components/ngx-bootstrap-icons/ngx-bootstrap-icons.component.ts
+++ projects/ngx-bootstrap-icons-lib/src/lib/components/ngx-bootstrap-icons/ngx-bootstrap-icons.component.ts
@@ -6,22 +6,23 @@ import {
   Input,
   OnChanges,
   SimpleChanges,
-} from "@angular/core";
+} from '@angular/core';
+
 import {
   IModuleConfigOptions,
   MODULE_CONFIG_TOKEN,
-} from "../../config/module.config";
-import { IconNamesEnum } from "../../enums/icon-names.enum";
-import { toCamelCase } from "../../internal/utils/toCamelCase";
-import { Icons } from "../../providers/icon.provider";
-import { IconName } from "../../types/icon-name.type";
+} from '../../config/module.config';
+import { IconNamesEnum } from '../../enums/icon-names.enum';
+import { toCamelCase } from '../../internal/utils/toCamelCase';
+import { Icons } from '../../providers/icon.provider';
+import { IconName } from '../../types/icon-name.type';
 
 /**
  * Bootstrap icon component.
  */
 @Component({
-  selector: "i-bs, *[i-bs]",
-  template: "<ng-content></ng-content>",
+  selector: 'i-bs, *[i-bs]',
+  template: '<ng-content></ng-content>',
 })
 export class NgxBootstrapIconsLibComponent implements OnChanges {
   /** Icon name. */
@@ -40,7 +41,7 @@ export class NgxBootstrapIconsLibComponent implements OnChanges {
     private _elem: ElementRef,
     private _changeDetector: ChangeDetectorRef,
     @Inject(Icons) private _icons: Icons,
-    @Inject(MODULE_CONFIG_TOKEN) private _config: IModuleConfigOptions
+    @Inject(MODULE_CONFIG_TOKEN) private _config: IModuleConfigOptions,
   ) {}
 
   /**
@@ -52,7 +53,7 @@ export class NgxBootstrapIconsLibComponent implements OnChanges {
     // icons are provided as an array of objects because of "multi: true"
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const icons = Object.assign({}, ...(this._icons as any as object[]));
-    let svg = icons[toCamelCase(changes.name.currentValue)] || "";
+    let svg = icons[toCamelCase(changes.name.currentValue)] || '';
 
     if (!svg) {
       console.warn(`Icon not found: ${changes.name.currentValue}\n`);
@@ -73,14 +74,14 @@ export class NgxBootstrapIconsLibComponent implements OnChanges {
     }
 
     if (this.resetDefaultDimensions) {
-      svg = svg.replace('width="16"', "");
-      svg = svg.replace('height="16"', "");
+      svg = svg.replace('width="16"', '');
+      svg = svg.replace('height="16"', '');
     }
 
-    if (this.width && svg.includes("width")) {
+    if (this.width && svg.includes('width')) {
       svg = svg.replace('width="16"', `width="${this.width}"`);
     }
-    if (this.height && svg.includes("height")) {
+    if (this.height && svg.includes('height')) {
       svg = svg.replace('height="16"', `height="${this.height}"`);
     }
 
--- projects/ngx-bootstrap-icons-lib/src/lib/types/color-theme.type.ts
+++ projects/ngx-bootstrap-icons-lib/src/lib/types/color-theme.type.ts
@@ -4,14 +4,14 @@
  * {@link https://getbootstrap.com/docs/5.0/utilities/colors/#color}
  */
 export type ColorThemeType =
-  | "text-primary"
-  | "text-secondary"
-  | "text-success"
-  | "text-danger"
-  | "text-warning"
-  | "text-info"
-  | "text-dark"
-  | "text-body"
-  | "text-muted"
-  | "text-black-50"
-  | "text-white-50";
+  | 'text-primary'
+  | 'text-secondary'
+  | 'text-success'
+  | 'text-danger'
+  | 'text-warning'
+  | 'text-info'
+  | 'text-dark'
+  | 'text-body'
+  | 'text-muted'
+  | 'text-black-50'
+  | 'text-white-50';
