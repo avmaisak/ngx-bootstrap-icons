ace0dd19c00e6aa7d764d8e50d7a58689524f0fc
tayamba mwanza <tayambamwanza@gmail.com>
2021-09-14 16:14:30 +0200

    feat: export types

--- projects/ngx-bootstrap-icons-lib/src/public-api.ts
+++ projects/ngx-bootstrap-icons-lib/src/public-api.ts
@@ -8,3 +8,6 @@ export * from './lib/enums/color-theme.enum';
 export * from './lib/enums/icon-names.enum';
 export * from './lib/icons';
 export * from './lib/ngx-bootstrap-icons.module';
+export * from './lib/types/color-theme.type';
+export * from './lib/types/icon-name.type';
+
